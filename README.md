65538bc8ce4be916b36b5dc5bc2eabb6848d547b
akindutire <akinsamuel33@gmail.com>
2022-07-26 01:47:05 +0100

    --

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<include a CircleCI status badge, here>
+[![CircleCI](https://dl.circleci.com/status-badge/img/gh/akindutire/devops_ml/tree/main.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/akindutire/devops_ml/tree/main)
 
 ## Project Overview
 
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=zil0/uda:ml_ops
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlops --image=${dockerpath} --port=80 --labels app=mlops
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlops 8000:80
